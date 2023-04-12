a4e0fd0d4caefe4a3d9598125bbb3aa09fa051a3
gunelediyarova <gunelbaghishova@gmail.com>
2023-04-12 13:55:32 -0400

    day 27 work Wrapper classes ArrayList intro

---
+++ src/day27_wrapper_araylist/WrapperClassIntro.java
@@ -0,0 +1,15 @@
+package day27_wrapper_araylist;
+
+import java.util.Arrays;
+
+public class WrapperClassIntro {
+
+    public static void main(String[] args) {
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('A'));
+
+
+
+
+    }
+}
