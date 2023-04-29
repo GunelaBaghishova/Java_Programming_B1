8e0c177892ddeae28804e62801538b3822508198
gunelediyarova <gunelbaghishova@gmail.com>
2023-04-29 15:02:39 -0400

    day36_works

---
+++ src/day36_inheritance/hiding/Person.java
@@ -0,0 +1,23 @@
+package day36_inheritance.hiding;
+
+public class Person {
+
+    String name = "Tom Jerry";
+}
+
+class Child extends Person{
+
+//    String name = "Jame Gouslin";
+
+}
+
+class Runner{
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.name);
+        System.out.println("------------------------------");
+
+       Child child = new Child();
+        System.out.println(child.name);
+    }
+}
