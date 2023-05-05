fe02ea814dceec8e3e277b97169990c811196377
gunelediyarova <gunelbaghishova@gmail.com>
2023-05-04 22:05:02 -0400

    day38_work

---
+++ src/day38_b_polymorphism/animal/Reptile.java
@@ -0,0 +1,10 @@
+package day38_b_polymorphism.animal;
+
+public class Reptile extends Animal{
+
+    int numOfClaws;
+
+    public void eat(){
+        System.out.println("Reptile is eating");
+    }
+}
